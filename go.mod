d9f4bf95688150c8d01b97a7bc497676b00076b3
lynnzhang89 <zhanglin_8905@outlook.com>
2023-07-29 17:14:02 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lynnzhang89/cloud-native-1
+
+go 1.20
--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
+func handle(writer http.ResponseWriter, request *http.Request) {
+	headers := request.Header
+	for key, value := range headers {
+		writer.Header().Set(key, value[0])
+	}
+	version := os.Getenv("VERSION")
+	log.Printf("version: %s", version)
+	if version != "" {
+		writer.Header().Set("VERSION", version)
+	}
+
+	log.Printf("client ip: %s, status code: %d", request.RemoteAddr, http.StatusOK)
+}
+
+func healthz(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte("OK"))
+}
+
+func main() {
+	http.HandleFunc("/", handle)
+	http.HandleFunc("/healthz", healthz)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("http start failed err:", err)
+		return
+	}
 }
